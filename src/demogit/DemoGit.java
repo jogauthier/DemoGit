446d89d25acbe006b69b27e35195aba04824a6aa
jogauthier <gauthier.jonathan.2@courrier.uqam.ca>
2015-01-22 23:13:58 -0300

    verification arguments

--- src/demogit/DemoGit.java
+++ src/demogit/DemoGit.java
@@ -15,7 +15,11 @@ public class DemoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+       if(args.length !=1) {
+           System.out.println("Wrong args");
+           System.exit(1);
+        }
+       
         System.out.println("Hello" + args[0]+ "!");
     }
-    
 }
